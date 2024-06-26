4673bb591404b50efecc1bd525b3e509de2f2b8c
sakshichandrakar <sakshichandrakar2911@gmail.com>
2024-06-26 23:01:03 +0530

    index and contactus page setup

---
+++ resources/views/layout/footerLink.blade.php
@@ -0,0 +1,24 @@
+<!--jquery js-->
+    <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+    <!--bootstrap js-->
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+    <!--owl-carousal-->
+    <script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+    <!--progress-bar js-->
+    <script src="{{asset('assets/js/progressbar.js')}}"></script>
+    <!--isotope js-->
+    <script src="{{asset('assets/js/isotope.pkgd.min.js')}}"></script>
+    <!--countTo js-->
+    <script src="{{asset('assets/js/jquery.countTo.min.js')}}"></script>
+    <!--Maginfic-Popup js-->
+    <script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+    <!-- Animation JS -->
+    <script src="{{asset('assets/js/wow.min.js')}}"></script>
+    <!-- Rev-Slider -->
+    <script src="{{asset('assets/rev/js/rbtools.min.js')}}"></script>
+    <script src="{{asset('assets/rev/js/rs6.min.js')}}"></script>
+    <script src="{{asset('assets/js/rev-custom.js')}}"></script>
+    <!--custom js-->
+    <script src="{{asset('assets/js/custom.js')}}"></script>
+    <script>'undefined'=== typeof _trfq || (window._trfq = []);'undefined'=== typeof _trfd && (window._trfd=[]),_trfd.push({'tccl.baseHost':'secureserver.net'},{'ap':'cpbh-mt'},{'server':'sg2plmcpnl492384'},{'dcenter':'sg2'},{'cp_id':'9858662'},{'cp_cache':''},{'cp_cl':'8'})</script>
+    <script src='../../../../img1.wsimg.com/signals/js/clients/scc-c2/scc-c2.min.js'></script>
